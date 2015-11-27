62aa103a3b0bffaff4f6e28394fbd1618b65ab5e
Nick Wan <nickwanhere@gmail.com>
2015-11-27 11:21:23 +0800

    update main appjs

---
+++ app.js
@@ -0,0 +1,54 @@
+var cloudinary = require('cloudinary');
+var cloudinary_config = require('./config.js');
+
+console.log(cloudinary_config.CLOUD_NAME);
+
+cloudinary.config({ 
+  cloud_name: cloudinary_config.CLOUD_NAME, 
+  api_key: cloudinary_config.API_KEY, 
+  api_secret: cloudinary_config.API_SECRET 
+});
+
+console.log('Start Cloudwasher');
+
+
+var cursor = '';
+
+var CronJob = require('cron').CronJob;
+var job = new CronJob({
+  cronTime: '*/20 * * * * *',
+  onTick: function() {
+    
+
+    cloudinary.api.resources(function(result){
+          
+          var public_ids = [];
+          cursor = result.next_cursor;
+
+          result.resources.forEach(function(resource){
+
+              console.log('Public ID '+resource.public_id );
+              public_ids.push(resource.public_id )
+
+          });
+
+
+              cloudinary.api.delete_resources(public_ids, function(del_result){
+
+                  console.log(del_result);
+
+
+              }, {keep_original:1});
+
+      },{
+      direction:1,
+      max_results:100,
+      next_cursor:cursor
+      });
+
+
+  },
+  start: true,
+  timeZone: 'Asia/Hong_kong'
+});
+job.start();
---
+++ config.js
@@ -0,0 +1,8 @@
+var cloudinary_config = {};
+
+
+cloudinary_config.CLOUD_NAME = 'XXXXX';
+cloudinary_config.API_KEY = 'XXXXXXXXX'; 
+cloudinary_config.API_SECRET = 'XXXXXXXXXX';
+
+module.exports = cloudinary_config;
